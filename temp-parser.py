30dc1e65f43ea6161a8c84f5b5f1f8c2e6580fff
Liam Reynolds <liam@bitratchet.com>
2025-08-07 14:59:29 -0700

    MAJOR CHANGE TO FUNCTIONALITY (#3)
    This script will now separate tables if there is too large a time gap
    between two entries, resulting in multiple smaller tables/graphs.
    This change is to allow for a single log file to not result in a very visually
    confusing graph if that log file is not being continuously updated.
    
    IMPORTANT: the --outputfile argument HAS BEEN DEPRECATED. This is because multiple
    output files are now potentially being created and are instead stored in the new Output
    directory.
    
    TODO: allow for dynamic naming of the Output directory.

--- temp-parser.py
+++ temp-parser.py
@@ -9,6 +9,24 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import os
+
+
+def _next_table_name(num: int) -> str:
+    p_num = str(num).zfill(3)
+    return "Output/temp_csv_" + p_num + ".csv"
+
+
+def _next_png_name(num: int) -> str:
+    p_num = str(num).zfill(3)
+    return "Output/temperature_data_" + p_num + ".png"
+
+
+def _switch_out_file(tables, num: int):
+    tables.append(open(_next_table_name(num), 'w+', newline=''))
+    out_writer = csv.writer(tables[num])
+    out_writer.writerow(['datetime', 'device', 'temperature'])
+    return out_writer
 
 
 def main():
@@ -16,14 +34,17 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--inputfile", help="The path to the input file",
                         default=None)
-    parser.add_argument("--outputfile", help="The name of the output file",
-                        default="temp-csv-000.csv")
     parser.add_argument("-g", "--graph", help="display a graph of the data",
                         default=False, action="store_true")
-    parser.add_argument("-s", "--save_output", help="saves the output graphs as PNGs",
+    parser.add_argument("-s", "--save_output",
+                        help="saves the output graphs as PNGs",
                         default=False, action="store_true")
-    parser.add_argument("-r", "--report", help="create an index.html page with the graph(s)",
+    parser.add_argument("-r", "--report",
+                        help="create an index.html page with the graph(s)",
                         default=False, action="store_true")
+    parser.add_argument("-c", "--cutoff", default=20,
+                        help="max time in minutes between two table entries")
+    # TODO: ADD A --HELP ARGUMENT
     args = parser.parse_args()
 
     # Try to safely open the files passed to us
@@ -32,17 +53,15 @@ def main():
     except OSError:
         print("Unable to open input file " + args.filename)
         exit(1)
+
     try:
-        out_file = open(args.outputfile, "w", newline='')
+        os.makedirs("Output", exist_ok=True)
     except OSError:
-        print("Unable to create open/create new output file " + args.output)
+        print("Error creating output directory")
         exit(1)
-
-    # Files are safely open. Create a csvwriter to neatly write our output
-    out_writer = csv.writer(out_file)
-    out_writer.writerow(['datetime', 'device', 'temperature'])
-
     """
+    TODO: update this documentation to explain the multi-charting process
+
     Look through each line of the input file. Each line should be in the form:
     MON DD HH:MM:SS .(*?) [({"A":"DEVICENAME", "T":(TEMP|null)})*?]
     The number of (A,T) pairs may differ from line to line.
@@ -51,14 +70,21 @@ def main():
     A line with n (A,T) pairs will be written into output file as n lines,
     with each (A,T) pair having its own line. This is for ease of graphing.
     """
+
+    out_tables = []
+    out_writer = _switch_out_file(out_tables, len(out_tables))
+    prev_date = None
     for line in in_file:
-        # filter out any line that we suspect does not match this format
-        # TODO: Separate dates with gaps of >20~ minutes into different plots
+        # Skip lines that we suspect don't fit our pattern
         if line.find("heatmon") == -1 or line.find("ERROR") != -1:
             continue
         mon = time.strptime(line[:3], '%b').tm_mon
         t = pd.to_datetime(line[4:15], format='%d %H:%M:%S')
         t = t.replace(month=mon, year=time.localtime().tm_year)
+        if prev_date and (t - prev_date).seconds > (args.cutoff * 60):
+            # Switch to new table
+            out_writer = _switch_out_file(out_tables, len(out_tables))
+        prev_date = t
         device_matches = re.finditer(r'A":"(.*?)"', line)
         temp_matches = re.finditer(r'T":(.*?|null)}', line)
         devices = []
@@ -70,32 +96,33 @@ def main():
         for i in range(len(temps)):
             out_writer.writerow([t, devices[i], temps[i]])
 
-    # We are done with our input file and can safely close it
-    in_file.close()
-    out_file.close()
+    for file in out_tables:
+        file.flush()
+        file.close()
 
-    # If we are not graphing our data, safely terminate
-    if not args.graph and not (args.save_output or args.report):
-        exit(0)
-
-    # graph of data using panda's dataframe and matplotlib for visualization
-    df = pd.read_csv(args.outputfile, parse_dates=['datetime'])
-    fig, ax = plt.subplots()
-    for device, sub_df in df.groupby('device'):
-        sub_df.plot(ax=ax, x='datetime', y='temperature', label=device)
-    ax.legend(title='devices')
-    plt.legend(bbox_to_anchor=(1, 1))
-    plt.tight_layout()
-    if args.save_output:
-        plt.savefig("temp-graph-001.png") # TODO: dynamic naming
-    if args.graph:
-        plt.show(block=True)
+    for file_num in range(len(out_tables)):
+        file = out_tables[file_num]
+        df = pd.read_csv(file.name, parse_dates=['datetime'])
+        fig, ax = plt.subplots()
+        for device, sub_df in df.groupby('device'):
+            sub_df.plot(ax=ax, x='datetime', y='temperature', label=device)
+        ax.legend(title='devices')
+        plt.legend(bbox_to_anchor=(1, 1))
+        plt.tight_layout()
+        if args.save_output or args.report:
+            plt.savefig(_next_png_name(file_num))
+        if args.graph:
+            plt.show()
 
     if args.report:
         # write an index.html page with the graphs we made
-        with open('index.html', 'w') as f:
+        with open('temperature-report.html', 'w') as f:
             f.write("<!DOCTYPE HTML> \n <html> \n <b>")
-            f.write('<img src=\"temp-graph-001.png\" alt=\"graph 1\">')
+            for file_num in range(len(out_tables)):
+                f.write('<img src=\"' +
+                        _next_png_name(file_num) +
+                        '\" alt=\"graph 1\">')
             f.write("</b> \n </html>")
+
 
 main()
